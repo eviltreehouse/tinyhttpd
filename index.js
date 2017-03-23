3d9ed9c18d062be36d8f4963e2acaebced7f98db
Corey Sharrah <corey@eviltreehouse.com>
2017-03-23 09:49:29 -0700

    initial

---
+++ index.js
@@ -0,0 +1,76 @@
+        var HttpDispatcher = require('httpdispatcher');
+        var http           = require('http');
+        var dispatcher     = new HttpDispatcher();
+
+	var ejs = require('ejs');
+
+        dispatcher.setStatic('/resources');
+        dispatcher.setStaticDirname('static');
+
+        dispatcher.onGet("/page1", function(req, res) {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Page One');
+        });
+
+        dispatcher.onPost("/page2", function(req, res) {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Page Two');
+        });
+
+	dispatcher.onGet("/args", function(req, res) {
+		res.setHeader('X-Foobar', '123');
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(req.params);
+	});
+
+	dispatcher.onPost("/args", function(req, res) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+
+		var json;
+		console.log(req.headers["content-type"]);
+
+		if (req.headers["content-type"] == 'application/json' && Object.keys(req.params)[0].match(/^\{/)) {
+			console.log("Its JSON");
+			try {
+				json = JSON.parse( Object.keys(req.params)[0] );
+			} catch(e) {
+			}
+			console.log(json);
+		}
+
+		res.end(json ? json.test : "null");
+	});
+
+	dispatcher.onGet("/ejs", function(req, res) {
+		var view = "<html><body><p>The value of foobar is <%= foobar %>.</p></body></html>";
+		
+		var output = '';
+		try {
+			output = ejs.render(view, req.params);
+		} catch(e) {
+			output = `[ Error rendering view: ${e.message} ]`;
+		}
+
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end( output );
+	});
+
+        dispatcher.beforeFilter(/\//, function(req, res, chain) { //any url
+                console.log("Before filter");
+                chain.next(req, res, chain);
+        });
+
+        dispatcher.afterFilter(/\//, function(req, res, chain) { //any url
+                console.log("After filter");
+                chain.next(req, res, chain);
+        });
+
+        dispatcher.onError(function(req, res) {
+                res.writeHead(404);
+                res.end('File Not Found');
+        });
+
+        http.createServer(function (req, res) {
+                dispatcher.dispatch(req, res);
+        }).listen(1337, '127.0.0.1');
+
