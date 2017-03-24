1ca6247cc40252be67d911b6645daa93b7cb0bd7
Corey Sharrah <corey@eviltreehouse.com>
2017-03-24 14:22:33 -0700

    adding parse and provide tests

---
+++ test/02-base-parse-and-provide.test.js
@@ -0,0 +1,99 @@
+var util = require('./util');
+var assert = require('simple-assert');
+
+var TinyHttpd = require('../tinyhttpd');
+
+const INTERFACE = '127.0.0.1';
+const BIND_PORT = '19891';
+const TEST_CONFIG = {
+	'basedir': process.cwd() + '/test/02-test-base',
+	'interface': INTERFACE,
+	'port': BIND_PORT
+};
+
+describe("Parsing/Providing Tests", () => {
+	var th = null;
+	
+	before('Starting tinyhttpd', function(done) {
+		th = new TinyHttpd(TEST_CONFIG).then((self) => {
+			th = self;
+			
+			th.start().then(() => {
+				done();	
+			}, done) 
+		}, done);
+	});
+	
+	after('Stopping tinyhttpd', function(done) {
+		th.stop().then(() => {
+			th = null;
+			done();
+		}, done);
+	});
+	
+	it("Should init and start up", () => {
+		assert(th.started);
+	})
+	
+	it("Should serve requests that involve handler modules and EJS", (done) => {
+		util.get('/', {}).then((resp) => {
+			try {
+				assert(resp.code == 200, 'HTTP code is not 200');
+				assert(resp.body.match(/foobar sanchez 123/));
+				done();
+			} catch(e) {
+				done(e.message);
+			}
+		}, done);
+	});	
+	
+	it("Should be able to serve handlers within subdirs", (done) => {
+		util.get('/sd/', {}).then((resp) => {
+			try {
+				assert(resp.code == 200, 'Http code is not 200');
+				assert(resp.body.match(/In subdir/), 'Body not as expected');
+				done();
+			} catch(e) {
+				done(e.message);
+			}
+		});
+	});
+	
+	it("Should be able to serve index handlers within subdirs w/o leading slash", (done) => {
+		util.get('/sd', {}).then((resp) => {
+			try {
+				assert(resp.code == 200, 'Http code is not 200');
+				assert(resp.body.match(/In subdir/), 'Body not as expected');
+				done();
+			} catch(e) {
+				done(e.message);
+			}
+		});
+	});	
+	
+	it("Should be able to access provisions from handler modules", (done) => {
+		th.provide('secret', 'sosecret');
+		util.get('/checkmy', {}).then((resp) => {
+			try {
+				assert(resp.code == 200, 'HTTP code is not 200');
+				assert(resp.body.match(/My secret is sosecret/), 'Body is not as expected');
+				done();
+			} catch(e) {
+				done(e.message);
+			}
+		}, done);		
+	});
+	
+	it("Should be able to access provisions from within views directly", (done) => {
+		th.provide('secret', 'sosecret');
+		util.get('/checkmy-view', {}).then((resp) => {
+			try {
+				assert(resp.code == 200, 'HTTP code is not 200');
+				assert(resp.body.match(/My secret is sosecret/), 'Body is not as expected');
+				done();
+			} catch(e) {
+				done(e.message);
+			}
+		}, done);		
+	});	
+});
